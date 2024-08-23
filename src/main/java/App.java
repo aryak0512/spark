b7e0632bf0a81e3c2497b7d444ef0c11db03e4cd
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2024-08-23 18:06:35 +0530

    Explore Pair RDD function added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,9 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Tuple2;
@@ -26,27 +28,54 @@ public class App {
 
         exploreCount(nums, sparkContext);
         exploreReduce(nums, sparkContext);
-        exploreMap(List.of(2,35,46), sparkContext);
+        exploreMap(List.of(2, 35, 46), sparkContext);
         exploreCount(nums, sparkContext);
-        exploreTuple(List.of(2,35,46), sparkContext);
+        exploreTuple(List.of(2, 35, 46), sparkContext);
         sparkContext.close();
+
+
+        explorePairRdd(sparkContext);
+    }
+
+    private static void explorePairRdd(JavaSparkContext sparkContext) {
+
+        List<String> originalLogs = new ArrayList<>();
+        originalLogs.add("INFO : Tuple (2,1.4142135623730951)");
+        originalLogs.add("INFO : Count is 3");
+
+        JavaRDD<String> rdd = sparkContext.parallelize(originalLogs);
+
+        PairFunction<String, String, String> pf = s -> {
+            var level = s.split(":")[0];
+            var content = s.split(":")[1];
+            return new Tuple2<>(level, content);
+        };
+
+        JavaPairRDD<String, String> pairRDD = rdd.mapToPair(pf);
+
+        // extracting keys and values
+        JavaRDD<String> keysRdd = pairRDD.keys();
+        JavaRDD<String> vaulesRdd = pairRDD.values();
+
     }
 
     /**
      * Simple count operation
-     * @param nums the dataset to operate on
+     *
+     * @param nums         the dataset to operate on
      * @param sparkContext the context
      */
     private static void exploreCount(List<Double> nums, JavaSparkContext sparkContext) {
         JavaRDD<Double> rdd = sparkContext.parallelize(nums);
         int count = rdd.map(n -> 1)
-                       .reduce((a, b) -> a + b);
+                .reduce((a, b) -> a + b);
         log.info("Count is : {}", count);
     }
 
     /**
      * Simple reduce operation, demonstrating addition of elements
-     * @param nums the dataset to operate on
+     *
+     * @param nums         the dataset to operate on
      * @param sparkContext the context
      */
     private static void exploreReduce(List<Double> nums, JavaSparkContext sparkContext) {
@@ -59,13 +88,14 @@ public class App {
 
     /**
      * Simple reduce operation, demonstrating addition of elements
-     * @param nums the dataset to operate on
+     *
+     * @param nums         the dataset to operate on
      * @param sparkContext the context
      */
     private static void exploreMap(List<Integer> nums, JavaSparkContext sparkContext) {
         // build a RDD
         JavaRDD<Integer> rdd = sparkContext.parallelize(nums);
-        rdd.map(Math::sqrt).collect().forEach(e -> log.info("Square Root : {}", e ));
+        rdd.map(Math::sqrt).collect().forEach(e -> log.info("Square Root : {}", e));
     }
 
 
