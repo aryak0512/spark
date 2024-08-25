f5259296fc1d909c7f4ba9c893bce5bd8ae295a1
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2024-08-25 11:32:47 +0530

    Pipeline added

--- src/main/java/HousingPriceAnalysis.java
+++ src/main/java/HousingPriceAnalysis.java
@@ -1,3 +1,4 @@
+import org.apache.spark.ml.*;
 import org.apache.spark.ml.evaluation.RegressionEvaluator;
 import org.apache.spark.ml.feature.OneHotEncoder;
 import org.apache.spark.ml.feature.StringIndexer;
@@ -13,8 +14,6 @@ import org.apache.spark.sql.Row;
 
 import java.util.Map;
 
-import static org.apache.spark.sql.functions.col;
-
 public class HousingPriceAnalysis {
 
     public static void main(String[] args) {
@@ -25,47 +24,36 @@ public class HousingPriceAnalysis {
                 .option("inferSchema", true) // since we need only numeric types
                 .csv(path);
 
+        dataset = dataset.withColumnsRenamed(Map.of("price","label"));
+
+        // split into 80% training & testing data and 20% holdOut data
+        Dataset<Row> [] sets = dataset.randomSplit(new double[] {0.8, 0.2});
+        Dataset<Row> trainingAndTestingData = sets[0];  // test data will be for tuning the values of regParam and elasticNetParam
+        Dataset<Row> holdOutData = sets[1];             // for independent validation
+
         // handling non-numeric fields using vectors
         StringIndexer conditionIndexer = new StringIndexer();
         conditionIndexer.setInputCol("condition");
         conditionIndexer.setOutputCol("conditionIndex");
-        dataset = conditionIndexer.fit(dataset).transform(dataset);
 
         StringIndexer gradeIndexer = new StringIndexer();
         gradeIndexer.setInputCol("grade");
         gradeIndexer.setOutputCol("gradeIndex");
-        dataset = gradeIndexer.fit(dataset).transform(dataset);
 
         StringIndexer zipcodeIndexer = new StringIndexer();
         zipcodeIndexer.setInputCol("zipcode");
         zipcodeIndexer.setOutputCol("zipcodeIndex");
-        dataset = zipcodeIndexer.fit(dataset).transform(dataset);
 
         OneHotEncoder oneHotEncoder = new OneHotEncoder();
         oneHotEncoder.setInputCols(new String[]{"conditionIndex","gradeIndex","zipcodeIndex"});
         oneHotEncoder.setOutputCols(new String[]{"conditionVector","gradeVector","zipcodeVector"});
-        dataset = oneHotEncoder.fit(dataset).transform(dataset);
-        dataset.show();
-
 
         // prepare the vector of features
         VectorAssembler vectorAssembler = new VectorAssembler();
         vectorAssembler.setInputCols(new String[]{"bedrooms", "bathrooms", "sqft_living", "sqft_lot","floors","grade","conditionVector","gradeVector","zipcodeVector","waterfront"});
         vectorAssembler.setOutputCol("features");
 
-        Dataset<Row> inputData = vectorAssembler.transform(dataset)
-                .select(col("price"), col("features")) // select only required features
-                .withColumnsRenamed(Map.of("price","label"));
-
-
-        // split into 80% training & testing data and 20% holdOut data
-        Dataset<Row> [] sets = inputData.randomSplit(new double[] {0.8, 0.2});
-
-        Dataset<Row> trainingAndTestingData = sets[0];  // test data will be for tuning the values of regParam and elasticNetParam
-        Dataset<Row> holdOutData = sets[1];             // for independent validation
-
         LinearRegression linearRegression = new LinearRegression();
-
         // adding the regParam and elasticNetParam params
         ParamGridBuilder paramGridBuilder = new ParamGridBuilder();
         // these are some sophisticated values we add for tuning the accuracy of the model
@@ -80,33 +68,36 @@ public class HousingPriceAnalysis {
                 .setEstimatorParamMaps(paramMap)
                 .setEstimator(linearRegression);
 
-        // feed the training data to the model
-        TrainValidationSplitModel splitModel = split.fit(trainingAndTestingData);
+        // fit and transform pipeline - a production standard way
+        Pipeline pipeline = new Pipeline();
+        pipeline.setStages(new PipelineStage[]{conditionIndexer, gradeIndexer, zipcodeIndexer, oneHotEncoder, vectorAssembler, split});
+        PipelineModel pipelineModel = pipeline.fit(dataset);
+
+        // run the pipeline on holdoutData and get the actual predictions
+        Dataset<Row> holdOutResults = pipelineModel.transform(holdOutData);
+        holdOutResults.show();
+        holdOutResults = holdOutResults.drop("prediction");
 
         // pick the best model
+        TrainValidationSplitModel splitModel = (TrainValidationSplitModel) pipelineModel.stages()[5];
         LinearRegressionModel model = (LinearRegressionModel) splitModel.bestModel();
-
-        double r2Training = model.summary().r2();
-        double rmseTraining = model.summary().rootMeanSquaredError();
-        System.out.println("R2 training : " + r2Training + " | RMSE training : " + rmseTraining);
-
-        // test the accuracy by passing the test data to the model
-        model.transform(holdOutData).show();
 
         // evaluate accuracy
         // R square -> between 0 and 1, bigger the better
         // RMSE -> smaller the better
+        double r2Training = model.summary().r2();
+        double rmseTraining = model.summary().rootMeanSquaredError();
+        System.out.println("R2 training : " + r2Training + " | RMSE training : " + rmseTraining);
 
-        double r2Test = model.evaluate(holdOutData).r2();
-        double rmseTest = model.evaluate(holdOutData).rootMeanSquaredError();
+        double r2Test = model.evaluate(holdOutResults).r2();
+        double rmseTest = model.evaluate(holdOutResults).rootMeanSquaredError();
         System.out.println("R2 testing : " + r2Test + " | RMSE testing : " + rmseTest);
 
         // selecting the features for the model based on high correlation (near to 1 or -1) 0 means non-correlated
         // note : this is done using original dataset and dropping useless fields
-
         dataset = dataset.drop("id", "date", "waterfront", "view", "condition", "grade", "yr_renovated", "zipcode", "lat", "long");
         for ( String columnName : dataset.columns() ){
-            var corr = dataset.stat().corr("price", columnName);
+            var corr = dataset.stat().corr("label", columnName);
             System.out.println("The correlation between price and " + columnName + " is : " + corr);
         }
 
